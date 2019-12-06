769ed09a5a47f219d8ce738448ab62a2b68de341
Yuji Sugiura <y.sugiura.0316@gmail.com>
2019-12-06 19:27:10 +0900

    Add sealed config

---
+++ src/config.js
@@ -0,0 +1,4 @@
+export const channelARN = 'arn:aws:kinesisvideo:ap-northeast-1:------------:channel/----------------/-------------';
+export const accessKeyId = '--------------------';
+export const secretAccessKey = '----------------------------------------';
+export const region = 'ap-northeast-1';
